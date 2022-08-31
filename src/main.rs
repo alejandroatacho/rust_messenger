7cbcac9ea7c3437fa6d2561999880959acf6a34f
Hinamizawa <hinamizawa.github@gmail.com>
2022-08-31 18:34:16 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::io;
 
 fn main() {
-    // let mut num = 0;
+    let mut num = 0;
     println!("Hello, Rust Messenger has been initialized!");
 
-    // while num == 1 {
-    println!("write down your answer/question below and press enter to continue: ");
-    let mut answer = String::new();
-    io::stdin()
-        .read_line(&mut answer)
-        .expect("Failed to read line");
-    println!("{}", answer);
-    // num += 1;
-    //}
+    {
+        while num != 1 {
+            println!("write down your answer/question below and press enter to continue: ");
+            let mut answer = String::new();
+            io::stdin()
+                .read_line(&mut answer)
+                .expect("Failed to read line");
+            // println!("{}", answer);
+
+            println!("write down your answer/question below and press enter to continue: ");
+            let mut answer = String::new();
+            io::stdin()
+                .read_line(&mut answer)
+                .expect("Failed to read line");
+            num += 1;
+        }
+    }
 }
